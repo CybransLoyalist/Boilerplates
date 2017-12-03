097a0ce8a1f4bb978255f447efd97889c3980453
CybransLoyalist <nendorel@gazeta.pl>
2017-12-03 13:23:20 +0100

    ReactReduxBootstrap4

---
+++ ReactReduxBootstrap4/src/components/app.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+      <div className="bg-dark text-light">React simple starter</div>
+      </div>
+    );
+  }
+}
